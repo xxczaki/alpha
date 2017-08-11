82b231d2d0c3682da6d197a7da4f8c7a87a1d4b4
Anthony <keppi@o2.pl>
2017-08-11 15:57:03 +0200

    Delete app.js

--- assets/js/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/js/particles.json', function() {
-  console.log('callback - particles.min.js config loaded');
-});
